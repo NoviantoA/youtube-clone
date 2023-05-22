462bc9768de68684835283fb96d44215e2d7ce6a
noviantoa <20082010132@student.upnjatim.ac.id>
2023-05-22 13:04:20 +0700

    setup project

---
+++ backend/src/test/java/com/noviantoanggoro/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noviantoanggoro.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
